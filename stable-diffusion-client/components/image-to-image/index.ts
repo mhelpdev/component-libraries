b2e63ad2c8f353c9e1ae7189aa60a2d7207f8287
mhelpdev <mhelp.dev@gmail.com>
2023-03-27 12:15:30 -0400

    Feat: image-to-image component

--- stable-diffusion-client/components/image-to-image/index.ts
+++ stable-diffusion-client/components/image-to-image/index.ts
@@ -1,16 +1,17 @@
 import { PropType, registerComponent } from '@uiflow/cli';
-import { grpc as GRPCWeb } from "@improbable-eng/grpc-web";
-import * as Generation from "../../generation/generation_pb";
-import { GenerationServiceClient } from "../../generation/generation_pb_service";
+import { grpc as GRPCWeb } from '@improbable-eng/grpc-web';
+import { Buffer } from 'buffer';
+import * as Generation from '../../generation/generation_pb';
+import { GenerationServiceClient } from '../../generation/generation_pb_service';
 import {
   buildGenerationRequest,
   executeGenerationRequest,
   onGenerationComplete,
-} from "../../helper/helpers";
+} from '../../helper/helpers';
 import { b64toBlob } from '../../helper/utils';
 
-export default registerComponent('text-to-image-ufw-component', {
-  name: 'Text To Image',
+export default registerComponent('image-to-image-ufw-component', {
+  name: 'Image To Image',
   props: [
     {
       name: 'api_key',
@@ -27,40 +28,40 @@ export default registerComponent('text-to-image-ufw-component', {
           {
             name: 'prompt',
             type: PropType.String,
-            value: 'A dream of a distant galaxy, by Caspar David Friedrich, matte painting trending on artstation HQ',
+            value: 'sketchy crayon outline on old paper of a rocket-ship launching from rolling greens with blue daisies and weeping willow trees under a blue alien sky',
           },
           {
-            name: 'width',
-            type: PropType.Number,
-            value: 512,
+            name: 'image',
+            type: PropType.String,
           },
-          {
-            name: 'height',
-            type: PropType.Number,
-            value: 512,
-          }
         ],
-        onEmit({ props, inputs, emit }) {
+        async onEmit({ props, inputs, emit }) {
           const metadata = new GRPCWeb.Metadata();
-          metadata.set("Authorization", "Bearer " + props.api_key);
+          metadata.set('Authorization', 'Bearer ' + props.api_key);
 
-          const client = new GenerationServiceClient("https://grpc.stability.ai", {});
-
-          const request = buildGenerationRequest("stable-diffusion-512-v2-1", {
-            type: "text-to-image",
+          const client = new GenerationServiceClient('https://grpc.stability.ai', {});
+          
+          const blob = await fetch(inputs.image).then(r => r.blob());
+          const arrayBuffer = await blob.arrayBuffer();
+          const imageBuffer = Buffer.from(arrayBuffer);
+          
+          const imageStrength = 0.35;
+          const request = buildGenerationRequest('stable-diffusion-512-v2-1', {
+            type: 'image-to-image',
             prompts: [
               {
-                text: inputs.prompt
+                text: inputs.prompt,
               },
             ],
-            width: inputs.width,
-            height: inputs.height,
+            stepScheduleStart: 1 - imageStrength,
+            initImage: imageBuffer,
+            seed: 1413160511,
             samples: 1,
-            cfgScale: 13,
-            steps: 25,
+            cfgScale: 8,
+            steps: 30,
             sampler: Generation.DiffusionSampler.SAMPLER_K_DPMPP_2M,
           });
-
+          
           executeGenerationRequest(client, request, metadata)
             .then(onGenerationComplete)
             .then((images: string[]) => {
@@ -71,7 +72,7 @@ export default registerComponent('text-to-image-ufw-component', {
                 const blobUrl = URL.createObjectURL(blob);
                 emit('onResult', { src: blobUrl });
               } else {
-                emit('onError', { error: 'Failed to make text-to-image request' });
+                emit('onError', { error: 'Failed to make image-to-image request' });
               }
             })
             .catch((error) => {
